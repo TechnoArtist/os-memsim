fcf7b95c12e978d7125faca1a68dd9ba186ce690
Sheila Pietila <piet9564@stthomas.edu>
2021-04-17 15:30:26 -0500

    Added virtaddr to page num/offset conversion, started brainstorming addEntry()

--- src/pagetable.cpp
+++ src/pagetable.cpp
@@ -1,4 +1,5 @@
 #include "pagetable.h"
+#include <math.h>
 
 PageTable::PageTable(int page_size)
 {
@@ -24,11 +25,17 @@ std::vector<std::string> PageTable::sortedKeys()
 	return keys;
 }
 
+/*
+    This is a method to create a fresh virtual page by assigning it to an empty frame. 
+    
+    Input: pid: The ID of the currently running process, used as an ingredient to find available frames. 
+    Input: page_number: The number of the virtual page being allocated, used as an ingredient to find available frames. 
+*/
 void PageTable::addEntry(uint32_t pid, int page_number)
 {
 	// Combination of pid and page number act as the key to look up frame number
 	std::string entry = std::to_string(pid) + "|" + std::to_string(page_number);
-
+    
 	int frame = 0; 
 	// Find free frame 
 		// NOTE free "frame"? Not free "page"? This is going user-to-mem, not mem-to-user
@@ -46,9 +53,9 @@ void PageTable::addEntry(uint32_t pid, int page_number)
 int PageTable::getPhysicalAddress(uint32_t pid, uint32_t virtual_address)
 {
 	// Convert virtual address to page_number and page_offset
-	// TODO: implement this!
-	int page_number = 0;
-	int page_offset = 0;
+    uint32_t n = (uint32_t)log2(_page_size); 
+    int page_number = (int)(virtual_address >> n); 
+    int page_offset = (int)((_page_size - 1) & virtual_address); 
 
 	// Combination of pid and page number act as the key to look up frame number
 	std::string entry = std::to_string(pid) + "|" + std::to_string(page_number);
